6dcbc73c132b4cf70fbae92d085a8057b0cb8c4f
Mike Perry <perryjm@users.noreply.github.com>
2021-10-16 00:18:57 -0600

    prettier

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import './App.css';
-import React, { Component } from 'react';
-import axios from 'axios';
-import MyTable from './MyTable';
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import MyTable from "./MyTable";
 
 class App extends Component {
-	state = {
-		data: [],
-		syslog: '',
-		syslogerr: '',
-	};
+  state = {
+    data: [],
+    syslog: "",
+    syslogerr: "",
+  };
 
-	componentDidMount() {
-		axios.get('https://ucapphealth.com').then(response => {
-			//  {
-			// 		"id": 21,
-			// 		"title": "test app",
-			// 		"url": "https://pawnle.utahcounty.gov/app/api/healthcheck/bad/endpoint",
-			// 		"notes": null,
-			// 		"repo": null,
-			// 		"statusCode": 404,
-			// 		"error": "Request failed with status code 404"
-			// },
-			response.data.data.map(entry => {
-				entry.status = entry.status === null ? 'GOOD' : 'FAILURE';
-				if (entry.error === '30 second response time') entry.status = 'WARNING';
-				return entry;
-			});
+  componentDidMount() {
+    axios.get("https://ucapphealth.com").then((response) => {
+      //  {
+      // 		"id": 21,
+      // 		"title": "test app",
+      // 		"url": "https://pawnle.utahcounty.gov/app/api/healthcheck/bad/endpoint",
+      // 		"notes": null,
+      // 		"repo": null,
+      // 		"statusCode": 404,
+      // 		"error": "Request failed with status code 404"
+      // },
+      response.data.data.map((entry) => {
+        entry.status = entry.status === null ? "GOOD" : "FAILURE";
+        if (entry.error === "30 second response time") entry.status = "WARNING";
+        return entry;
+      });
 
-			this.setState({
-				data: response.data.data,
-				syslog: response.data.syslog, //.split('\n'),
-				syslogerr: response.data.syslogerr, //.split('\n'),
-			});
-		});
-	}
+      this.setState({
+        data: response.data.data,
+        syslog: response.data.syslog, //.split('\n'),
+        syslogerr: response.data.syslogerr, //.split('\n'),
+      });
+    });
+  }
 
-	render() {
-		return <MyTable data={this.state.data} />;
-	}
+  render() {
+    return <MyTable data={this.state.data} />;
+  }
 }
 
 export default App;
--- src/MyTable.js
+++ src/MyTable.js
@@ -1,60 +1,64 @@
-import React from 'react';
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import React from "react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 
 function MyTable(props) {
-	if (!props.data || props.data.length === 0) {
-		return <p>There was no data returned from the server, this is likely an error.</p>;
-	}
-	return (
-		<Table border="1">
-			<Thead>
-				<Tr key="0" >
-					{Object.keys(props.data[0]).map(header => {
-						if (header !== 'url') {
-							return <Th>{header}</Th>;
-						}
-						return <div></div>;
-					})}
-				</Tr>
-			</Thead>
-			<Tbody>
-				{props.data.map(entry => {
-					return (
-						<Tr key={entry.id}>
-							{Object.keys(entry).map(column => {
-								if (column === 'repo') {
-									return (
-										<Td>
-											<a href="{entry[column]}">repo</a>
-										</Td>
-									);
-								} else if (column === 'status') {
-									if (entry[column] === 'GOOD') {
-										return <Td>🟢</Td>;
-									} else if (entry[column] === 'WARNING') {
-										return <Td>🟡</Td>;
-									} else if (entry[column] === 'FAILURE') {
-										return <Td>🔴</Td>;
-									}
-								} else if (column === 'title') {
-									return (
-										<Td>
-											<a href="{entry.url}">{entry[column]}</a>
-										</Td>
-									);
-								} else if (column === 'url') {
-									return <div></div>;
-								} else {
-									return <Td>{entry[column]}</Td>;
-								}
-								return <div></div>
-							})}
-						</Tr>
-					);
-				})}
-			</Tbody>
-		</Table>
-	);
+  if (!props.data || props.data.length === 0) {
+    return (
+      <p>
+        There was no data returned from the server, this is likely an error.
+      </p>
+    );
+  }
+  return (
+    <Table border="1">
+      <Thead>
+        <Tr key="0">
+          {Object.keys(props.data[0]).map((header) => {
+            if (header !== "url") {
+              return <Th>{header}</Th>;
+            }
+            return <div></div>;
+          })}
+        </Tr>
+      </Thead>
+      <Tbody>
+        {props.data.map((entry) => {
+          return (
+            <Tr key={entry.id}>
+              {Object.keys(entry).map((column) => {
+                if (column === "repo") {
+                  return (
+                    <Td>
+                      <a href="{entry[column]}">repo</a>
+                    </Td>
+                  );
+                } else if (column === "status") {
+                  if (entry[column] === "GOOD") {
+                    return <Td>🟢</Td>;
+                  } else if (entry[column] === "WARNING") {
+                    return <Td>🟡</Td>;
+                  } else if (entry[column] === "FAILURE") {
+                    return <Td>🔴</Td>;
+                  }
+                } else if (column === "title") {
+                  return (
+                    <Td>
+                      <a href="{entry.url}">{entry[column]}</a>
+                    </Td>
+                  );
+                } else if (column === "url") {
+                  return <div></div>;
+                } else {
+                  return <Td>{entry[column]}</Td>;
+                }
+                return <div></div>;
+              })}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
 }
 export default MyTable;
