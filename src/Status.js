e516588a981f5619102ebca662800e3a199527e3
Mike Perry <perryjm@users.noreply.github.com>
2021-11-12 23:48:07 -0700

    color-blind modifications

--- src/Status.js
+++ src/Status.js
@@ -1,48 +1,48 @@
-import React from "react";
-import "./Status.css";
-import { BsGithub } from "react-icons/bs";
+import React from 'react';
+import './Status.css';
+import { BsGithub } from 'react-icons/bs';
 
 function getStatusIcon(status) {
-  if (status === "GOOD") {
-    return "🟢";
-  } else if (status === "WARNING") {
-    return "🟡";
-  } else if (status === "FAILURE") {
-    return "🔴";
-  }
+	if (status === 'GOOD') {
+		return '🟢';
+	} else if (status === 'WARNING') {
+		return '⚠️';
+	} else if (status === 'FAILURE') {
+		return '⛔️';
+	}
 }
 
 function getTitleLink(entry) {
-  return <a href={entry.url}>{entry.title}</a>;
+	return <a href={entry.url}>{entry.title}</a>;
 }
 
 function getRepoLink(repo) {
-  return (
-    <a href={repo}>
-      <BsGithub /> repo
-    </a>
-  );
+	return (
+		<a href={repo}>
+			<BsGithub /> repo
+		</a>
+	);
 }
 
 function Status(props) {
-  return (
-    <div>
-      {props.data.map((entry) => {
-        return (
-          <div key={entry.id}>
-            <p>
-              <span className="title">{getStatusIcon(entry.status)} </span>
-              <span className="title">{getTitleLink(entry)} </span>
-              <span>{getRepoLink(entry.repo)}</span>
-              <span>{entry.notes} </span>
-              <span>{entry.statusCode} </span>
-              <span>{entry.error} </span>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div>
+			{props.data.map(entry => {
+				return (
+					<div key={entry.id}>
+						<p>
+							<span className="title">{getStatusIcon(entry.status)} </span>
+							<span className="title">{getTitleLink(entry)} </span>
+							<span>{getRepoLink(entry.repo)}</span>
+							<span>{entry.notes} </span>
+							<span>{entry.statusCode} </span>
+							<span>{entry.error} </span>
+						</p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default Status;
