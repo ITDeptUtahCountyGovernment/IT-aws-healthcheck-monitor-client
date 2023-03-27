30af23f50c5863e639da33ba412ce1f2693153f7
chloehltam <128411152+chloehltam@users.noreply.github.com>
2023-03-27 12:40:15 -0600

    refactor component classes to functions

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,117 +1,87 @@
-import React, { Component } from "react";
-import { BsGithub } from "react-icons/bs";
-import { months } from "../utils/utils.js";
-import "./Status.css";
+import React, { Component, useEffect, useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { months } from '../utils/utils.js';
+import './Status.css';
 const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
 
-class Status extends Component {
-  state = {
-    entry: {},
-    showDetails: false,
-  };
+const Status = props => {
+	const [entry, setEntry] = useState(props.entry);
+	const [showDetails, setShowDetails] = useState(false);
 
-  componentDidMount() {
-    this.setState({
-      entry: this.props.entry,
-      showDetails: this.props.showDetails,
-    });
-  }
+	const getStatusIcon = () => {
+		if (entry.status === 'GOOD') {
+			return '🟢';
+		} else if (entry.status === 'WARNING') {
+			return '⚠️';
+		} else if (entry.status === 'FAILURE') {
+			return '⛔️';
+		}
+	};
 
-  getStatusIcon() {
-    if (this.state.entry.status === "GOOD") {
-      return "🟢";
-    } else if (this.state.entry.status === "WARNING") {
-      return "⚠️";
-    } else if (this.state.entry.status === "FAILURE") {
-      return "⛔️";
-    }
-  }
+	const getSince = uptime => {
+		const since = new Date(Math.max(Date.now() - ONE_YEAR_MS, uptime));
+		return `${months[since.getMonth()]} ${since.getDate()} ${since.getFullYear()}`;
+	};
 
-  getSince(uptime) {
-    let since;
-    if (uptime > Date.now() - ONE_YEAR_MS) {
-      since = new Date(uptime);
-    } else {
-      since = new Date(Date.now() - ONE_YEAR_MS);
-    }
-    since = `${
-      months[since.getMonth()]
-    } ${since.getDate()} ${since.getFullYear()}`;
-    return since;
-  }
-
-  render() {
-    let extraNotes = <></>;
-    if (this.state.entry.notes) {
-      extraNotes = <li>Extra notes: {this.state.entry.notes}</li>;
-    }
-    if (this.state.showDetails) {
-      // more detailed view
-      return (
-        <div>
-          <p
-            onClick={() => {
-              this.setState({ showDetails: false });
-            }}
-          >
-            <span className="title">
-              <span>{this.getStatusIcon()}</span>{" "}
-              <span>{this.state.entry.title}</span>
-            </span>
-            <span>{this.state.entry.statusCode} </span>
-            <span>{this.state.entry.error} </span>
-          </p>
-          <div className="details">
-            <ul>
-              <li>
-                {this.state.entry.uptime}% uptime{" "}
-                <span className="small-text">
-                  (since {this.getSince(this.state.entry.uptimeTrackingStart)})
-                </span>
-              </li>
-              <li>
-                {this.state.entry.name} - {this.state.entry.lead}
-                (x{this.state.entry.ext}){" "}
-                <a href="mailto:{this.state.entry.email}">
-                  {this.state.entry.email}
-                </a>
-              </li>
-              <li>
-                View the repo on{" "}
-                <a href={this.state.entry.repo}>
-                  GitHub <BsGithub className="small-icon" />
-                </a>
-                .
-              </li>
-              <li>
-                See the targeted endpoint{" "}
-                <a href={this.state.entry.url}>here</a>.
-              </li>
-              {extraNotes}
-            </ul>
-          </div>
-        </div>
-      );
-    } else {
-      // less detailed view
-      return (
-        <div>
-          <p
-            onClick={() => {
-              this.setState({ showDetails: true });
-            }}
-          >
-            <span className="title">
-              <span>{this.getStatusIcon()}</span>{" "}
-              <span>{this.state.entry.title}</span>
-            </span>
-            <span>{this.state.entry.statusCode} </span>
-            <span>{this.state.entry.error} </span>
-          </p>
-        </div>
-      );
-    }
-  }
-}
+	let extraNotes = <></>;
+	if (entry.notes) {
+		extraNotes = <li>Extra notes: {entry.notes}</li>;
+	}
+	if (showDetails) {
+		// more detailed view
+		return (
+			<div>
+				<p
+					onClick={() => {
+						setShowDetails(false);
+					}}>
+					<span className="title">
+						<span>{getStatusIcon()}</span> <span>{entry.title}</span>
+					</span>
+					<span>{entry.statusCode} </span>
+					<span>{entry.error} </span>
+				</p>
+				<div className="details">
+					<ul>
+						<li>
+							{entry.uptime}% uptime <span className="small-text">(since {getSince(entry.uptimeTrackingStart)})</span>
+						</li>
+						<li>
+							{entry.name} - {entry.lead}
+							(x{entry.ext}) <a href="mailto:{this.state.entry.email}">{entry.email}</a>
+						</li>
+						<li>
+							View the repo on{' '}
+							<a href={entry.repo}>
+								GitHub <BsGithub className="small-icon" />
+							</a>
+							.
+						</li>
+						<li>
+							See the targeted endpoint <a href={entry.url}>here</a>.
+						</li>
+						{extraNotes}
+					</ul>
+				</div>
+			</div>
+		);
+	} else {
+		// less detailed view
+		return (
+			<div>
+				<p
+					onClick={() => {
+						setShowDetails(true);
+					}}>
+					<span className="title">
+						<span>{getStatusIcon()}</span> <span>{entry.title}</span>
+					</span>
+					<span>{entry.statusCode} </span>
+					<span>{entry.error} </span>
+				</p>
+			</div>
+		);
+	}
+};
 
 export default Status;
