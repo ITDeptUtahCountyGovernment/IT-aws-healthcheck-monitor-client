d627abb62b3ba80c9c884277e9147ba8f513f38e
chloehltam <128411152+chloehltam@users.noreply.github.com>
2023-03-27 12:40:15 -0600

    refactor component classes to functions

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
-import "./App.css";
-import React, { Component } from "react";
-import { BsGithub } from "react-icons/bs";
-import axios from "axios";
-import StatusCollection from "./components/StatusCollection";
-import Syslog from "./components/Syslog";
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
+import axios from 'axios';
+import StatusCollection from './components/StatusCollection';
+import Syslog from './components/Syslog';
 
-class App extends Component {
-  state = {
-    data: [],
-    syslog: "",
-    syslogerr: "",
-  };
+const App = () => {
+	const [data, setData] = useState([]);
+	const [syslog, setSyslog] = useState('');
+	const [syslogerr, setSyslogerr] = useState('');
 
-  componentDidMount() {
-    axios.get("https://ucapphealth.com").then((response) => {
-      response.data.data.map((entry) => {
-        entry.status = entry.status === null ? "GOOD" : "FAILURE";
-        if (entry.error && entry.error.includes("timeout of 30000ms exceeded"))
-          entry.status = "WARNING";
-        return entry;
-      });
+	useEffect(() => {
+		axios.get('https://ucapphealth.com').then(response => {
+			response.data.data.map(entry => {
+				entry.status = entry.status === null ? 'GOOD' : 'FAILURE';
+				if (entry.error && entry.error.includes('timeout of 30000ms exceeded')) entry.status = 'WARNING';
+				return entry;
+			});
 
-      this.setState({
-        data: response.data.data,
-        syslog: response.data.syslog,
-        syslogerr: response.data.syslogerr,
-      });
-    });
-  }
+			setData(response.data.data);
+			setSyslog(response.data.syslog);
+			setSyslogerr(response.data.syslogerr);
+		});
+	});
 
-  render() {
-    return (
-      <div className="container">
-        <h1>UC App Health</h1>
-        <p>
-          {" "}
-          Tap an app for details and{" "}
-          <a href="https://github.com/ITDeptUtahCountyGovernment/IT-aws-healthcheck-monitor-client/issues">
-            report any issues <BsGithub className="small-icon" />
-          </a>{" "}
-          Download <a href="http://ucapphealth.com/db.sqlite">db.sqlite</a>
-        </p>
+	return (
+		<div className="container">
+			<h1>UC App Health</h1>
+			<p>
+				{' '}
+				Tap an app for details and{' '}
+				<a href="https://github.com/ITDeptUtahCountyGovernment/IT-aws-healthcheck-monitor-client/issues">
+					report any issues <BsGithub className="small-icon" />
+				</a>{' '}
+				Download <a href="http://ucapphealth.com/db.sqlite">db.sqlite</a>
+			</p>
 
-        <StatusCollection data={this.state.data} />
-        <Syslog syslog={this.state.syslog} syslogerr={this.state.syslogerr} />
-      </div>
-    );
-  }
-}
+			<StatusCollection data={data} />
+			<Syslog syslog={syslog} syslogerr={syslogerr} />
+		</div>
+	);
+};
 
 export default App;
--- src/components/Syslog.js
+++ src/components/Syslog.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { CodeBlock, dracula } from "react-code-blocks";
+import React from 'react';
+import { CodeBlock, dracula } from 'react-code-blocks';
 
-function Syslog(props) {
-  return (
-    <div>
-      <h2>Stdout</h2>
-      <CodeBlock
-        text={props.syslog}
-        language="bash"
-        theme={dracula}
-        customStyle={{
-          height: "150px",
-          overflow: "scroll",
-        }}
-      />
-      <h2>Stderr</h2>
-      <CodeBlock
-        text={props.syslogerr}
-        language="bash"
-        theme={dracula}
-        customStyle={{
-          height: "150px",
-          overflow: "scroll",
-        }}
-      />
-    </div>
-  );
-}
+const Syslog = props => {
+	return (
+		<div>
+			<h2>Stdout</h2>
+			<CodeBlock
+				text={props.syslog}
+				language="bash"
+				theme={dracula}
+				customStyle={{
+					height: '150px',
+					overflow: 'scroll',
+				}}
+			/>
+			<h2>Stderr</h2>
+			<CodeBlock
+				text={props.syslogerr}
+				language="bash"
+				theme={dracula}
+				customStyle={{
+					height: '150px',
+					overflow: 'scroll',
+				}}
+			/>
+		</div>
+	);
+};
 
 export default Syslog;
