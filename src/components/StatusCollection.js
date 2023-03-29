a7f4285a45e9b45e1844eaaae58a62b91028f604
chloehltam <128411152+chloehltam@users.noreply.github.com>
2023-03-29 14:08:00 -0600

    remove border

--- src/components/StatusCollection.js
+++ src/components/StatusCollection.js
@@ -2,7 +2,7 @@ import Status from './Status';
 
 const StatusCollection = props => {
 	return (
-		<table className=" mb-8 rounded-lg border text-left dark:text-gray-400">
+		<table className=" mb-8 rounded-lg border-b border-l text-left dark:text-gray-400">
 			<thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
 					<th scope="col">App Name</th>
