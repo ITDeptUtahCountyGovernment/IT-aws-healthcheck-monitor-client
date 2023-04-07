b618be0c1a7853ad63b97ecfb1f7ca405441a738
chloehltam <128411152+chloehltam@users.noreply.github.com>
2023-04-07 11:53:50 -0600

    Donut charts labels

--- src/components/DonutCharts.js
+++ src/components/DonutCharts.js
@@ -1,57 +1,59 @@
 import { PieChart } from 'react-minimal-pie-chart';
 import colors from 'tailwindcss/colors';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const DonutCharts = props => {
+	console.log(JSON.stringify(colors));
 	return (
-		<div className="flex max-h-80 w-full justify-between space-x-10 px-3 md:px-0">
+		<div className="flex max-h-60 w-full justify-between space-x-10 px-3 md:px-20">
 			<PieChart
 				data={[
-					{ title: 'GOOD', value: props.teamstats.GOOD, color: colors.emerald['500'] },
-					{ title: 'WARNING', value: props.teamstats.WARNING, color: colors.slate['700'] },
-					{ title: 'FAILURE', value: props.teamstats.FAILURE, color: colors.slate['700'] },
+					{ value: props.teamstats.GOOD, color: colors.emerald['500'] },
+					{ value: props.teamstats.WARNING, color: colors.slate['700'] },
+					{ value: props.teamstats.FAILURE, color: colors.slate['700'] },
 				]}
 				lineWidth="10"
 				rounded={true}
-				label={() => props.teamstats.GOOD + ' GOOD'}
-				labelPosition="0"
-				labelStyle={{
-					fill: colors.emerald['500'],
-					fontSize: '8pt',
-				}}
-				animate={true}
-			/>
+				animate={true}>
+				<text dominant-baseline="central" x="50" y="50" dy="-10" text-anchor="middle" className="fill-good text-xl">
+					{props.teamstats.GOOD}
+				</text>
+				<text dominant-baseline="central" x="50" y="50" dy="10" text-anchor="middle" className=" fill-good/75 text-xs font-light">
+					GOOD
+				</text>
+			</PieChart>
 			<PieChart
 				data={[
-					{ title: 'GOOD', value: props.teamstats.GOOD, color: colors.slate['700'] },
-					{ title: 'WARNING', value: props.teamstats.WARNING, color: colors.yellow['300'] },
-					{ title: 'FAILURE', value: props.teamstats.FAILURE, color: colors.slate['700'] },
+					{ value: props.teamstats.GOOD, color: colors.slate['700'] },
+					{ value: props.teamstats.WARNING, color: colors.yellow['300'] },
+					{ value: props.teamstats.FAILURE, color: colors.slate['700'] },
 				]}
 				lineWidth="10"
 				rounded={true}
-				label={() => props.teamstats.WARNING + ' WARNING'}
-				labelPosition="0"
-				labelStyle={{
-					fill: colors.yellow['300'],
-					fontSize: '8pt',
-				}}
-				animate={true}
-			/>
+				animate={true}>
+				<text dominant-baseline="central" x="50" y="50" dy="-10" text-anchor="middle" className="fill-warning text-xl">
+					{props.teamstats.WARNING}
+				</text>
+				<text dominant-baseline="central" x="50" y="50" dy="10" text-anchor="middle" className=" fill-warning/50 text-xs font-light">
+					WARNING
+				</text>
+			</PieChart>
 			<PieChart
 				data={[
-					{ title: 'GOOD', value: props.teamstats.GOOD, color: colors.slate['700'] },
-					{ title: 'WARNING', value: props.teamstats.WARNING, color: colors.slate['700'] },
-					{ title: 'FAILURE', value: props.teamstats.FAILURE, color: colors.red['500'] },
+					{ value: props.teamstats.GOOD, color: colors.slate['700'] },
+					{ value: props.teamstats.WARNING, color: colors.slate['700'] },
+					{ value: props.teamstats.FAILURE, color: colors.red['500'] },
 				]}
 				lineWidth="10"
 				rounded={true}
-				label={() => props.teamstats.FAILURE + ' FAIL'}
-				labelPosition="0"
-				labelStyle={{
-					fill: colors.red['500'],
-					fontSize: '8pt',
-				}}
-				animate={true}
-			/>
+				animate={true}>
+				<text dominant-baseline="central" x="50" y="50" dy="-10" text-anchor="middle" className="fill-failure text-xl">
+					{props.teamstats.FAILURE}
+				</text>
+				<text dominant-baseline="central" x="50" y="50" dy="10" text-anchor="middle" className=" fill-failure/75 text-xs font-light">
+					FAIL
+				</text>
+			</PieChart>
 		</div>
 	);
 };
