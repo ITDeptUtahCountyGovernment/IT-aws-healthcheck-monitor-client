48a073149976b01b15ae1a639fa0023bca5b737e
PaxtonTerryDev <PaxtonTerryDev@gmail.com>
2023-10-26 17:15:56 -0600

    changed data request to include class, updated how system log data is rendered.  Changed update time to 1 minute

--- src/components/Syslog.js
+++ src/components/Syslog.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import { CodeBlock, dracula } from 'react-code-blocks';
+import React from "react";
+import { CodeBlock, dracula } from "react-code-blocks";
 
-const Syslog = props => {
-	return (
-		<div className="w-full text-xs ">
-			<div>
-				<h2 className="mb-2 mt-4">Stdout</h2>
-				<CodeBlock
-					text={props.syslog}
-					language="bash"
-					theme={dracula}
-					customStyle={{
-						height: '300px',
-						overflow: 'scroll',
-					}}
-				/>
-			</div>
-			<div>
-				<h2 className="mb-2 mt-8">Stderr</h2>
-				<CodeBlock
-					text={props.syslogerr}
-					language="bash"
-					theme={dracula}
-					customStyle={{
-						height: '300px',
-						overflow: 'scroll',
-					}}
-				/>
-			</div>
-		</div>
-	);
+const Syslog = (props) => {
+  const stdout = props.syslog.join("\n");
+  const stderr = props.syslogerr.join("\n");
+  return (
+    <div className="w-full text-xs ">
+      <div>
+        <h2 className="mb-2 mt-4">Stdout</h2>
+        <CodeBlock
+          text={stdout}
+          language="text"
+          theme={dracula}
+          customStyle={{
+            height: "300px",
+            overflow: "scroll",
+          }}
+        />
+      </div>
+      <div>
+        <h2 className="mb-2 mt-8">Stderr</h2>
+        <CodeBlock
+          text={stderr}
+          language="bash"
+          theme={dracula}
+          customStyle={{
+            height: "300px",
+            overflow: "scroll",
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Syslog;
