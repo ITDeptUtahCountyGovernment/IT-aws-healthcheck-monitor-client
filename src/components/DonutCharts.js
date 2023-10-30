45a228772a462f1f15e959f79f6c120701009f19
PaxtonTerryDev <PaxtonTerryDev@gmail.com>
2023-10-30 16:12:43 -0600

    Changed Donut Chart text, extended Sidebar

--- src/components/DonutCharts.js
+++ src/components/DonutCharts.js
@@ -1,82 +1,130 @@
-import { PieChart } from 'react-minimal-pie-chart';
-import colors from 'tailwindcss/colors';
+import { PieChart } from "react-minimal-pie-chart";
+import colors from "tailwindcss/colors";
 
-const DonutCharts = props => {
-	return (
-		<div className="flex max-h-80 w-full justify-between space-x-10 px-3 md:px-20">
-			<PieChart
-				data={[
-					{ value: props.teamstats.GOOD, color: 'rgba(16, 185, 129, 0.5)' },
-					{ value: props.teamstats.WARNING, color: 'rgba(51, 65, 85, 0.5)' },
-					{ value: props.teamstats.FAILURE, color: 'rgba(51, 65, 85, 0.5)' },
-				]}
-				lineWidth="25"
-				animate={true}>
-				<PieChart
-					data={[
-						{ value: props.teamstats.GOOD, color: colors.emerald['500'] },
-						{ value: props.teamstats.WARNING, color: colors.slate['700'] },
-						{ value: props.teamstats.FAILURE, color: colors.slate['700'] },
-					]}
-					lineWidth="10"
-					animate={true}>
-					<text dominantBaseline="central" x="50" y="50" dy="-10" textAnchor="middle" className="fill-good text-xl">
-						{props.teamstats.GOOD}
-					</text>
-					<text dominantBaseline="central" x="50" y="50" dy="10" textAnchor="middle" className=" fill-good/75 text-xs font-light">
-						GOOD
-					</text>
-				</PieChart>
-			</PieChart>
-			<PieChart
-				data={[
-					{ value: props.teamstats.GOOD, color: 'rgba(51, 65, 85, 0.5)' },
-					{ value: props.teamstats.WARNING, color: 'rgba(253, 224, 71, 0.5)' },
-					{ value: props.teamstats.FAILURE, color: 'rgba(51, 65, 85, 0.5)' },
-				]}
-				lineWidth="25"
-				animate={true}>
-				<PieChart
-					data={[
-						{ value: props.teamstats.GOOD, color: colors.slate['700'] },
-						{ value: props.teamstats.WARNING, color: colors.yellow['300'] },
-						{ value: props.teamstats.FAILURE, color: colors.slate['700'] },
-					]}
-					lineWidth="10"
-					animate={true}>
-					<text dominantBaseline="central" x="50" y="50" dy="-10" textAnchor="middle" className="fill-warning text-xl">
-						{props.teamstats.WARNING}
-					</text>
-					<text dominantBaseline="central" x="50" y="50" dy="10" textAnchor="middle" className=" fill-warning/50 text-xs font-light">
-						WARNING
-					</text>
-				</PieChart>
-			</PieChart>
-			<PieChart
-				data={[
-					{ value: props.teamstats.GOOD, color: 'rgba(51, 65, 85, 0.5)' },
-					{ value: props.teamstats.WARNING, color: 'rgba(51, 65, 85, 0.5)' },
-					{ value: props.teamstats.FAILURE, color: 'rgba(239, 68, 68, 0.5)' },
-				]}
-				lineWidth="25"
-				animate={true}>
-				<PieChart
-					data={[
-						{ value: props.teamstats.GOOD, color: colors.slate['700'] },
-						{ value: props.teamstats.WARNING, color: colors.slate['700'] },
-						{ value: props.teamstats.FAILURE, color: colors.red['500'] },
-					]}
-					lineWidth="10"
-					animate={true}>
-					<text dominantBaseline="central" x="50" y="50" dy="-10" textAnchor="middle" className="fill-failure text-xl">
-						{props.teamstats.FAILURE}
-					</text>
-					<text dominantBaseline="central" x="50" y="50" dy="10" textAnchor="middle" className=" fill-failure/75 text-xs font-light">
-						FAIL
-					</text>
-				</PieChart>
-			</PieChart>
-		</div>
-	);
+const DonutCharts = (props) => {
+  return (
+    <div className="flex max-h-80 w-full justify-between space-x-10 px-3 md:px-20">
+      <PieChart
+        data={[
+          { value: props.teamstats.GOOD, color: "rgba(16, 185, 129, 0.5)" },
+          { value: props.teamstats.WARNING, color: "rgba(51, 65, 85, 0.5)" },
+          { value: props.teamstats.FAILURE, color: "rgba(51, 65, 85, 0.5)" },
+        ]}
+        lineWidth="25"
+        animate={true}
+      >
+        <PieChart
+          data={[
+            { value: props.teamstats.GOOD, color: colors.emerald["500"] },
+            { value: props.teamstats.WARNING, color: colors.slate["700"] },
+            { value: props.teamstats.FAILURE, color: colors.slate["700"] },
+          ]}
+          lineWidth="10"
+          animate={true}
+        >
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="-10"
+            textAnchor="middle"
+            className="fill-good text-xl"
+          >
+            {props.teamstats.GOOD}
+          </text>
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="10"
+            textAnchor="middle"
+            className=" fill-good/75 text-xs font-light"
+          >
+            GOOD
+          </text>
+        </PieChart>
+      </PieChart>
+      <PieChart
+        data={[
+          { value: props.teamstats.GOOD, color: "rgba(51, 65, 85, 0.5)" },
+          { value: props.teamstats.WARNING, color: "rgba(253, 224, 71, 0.5)" },
+          { value: props.teamstats.FAILURE, color: "rgba(51, 65, 85, 0.5)" },
+        ]}
+        lineWidth="25"
+        animate={true}
+      >
+        <PieChart
+          data={[
+            { value: props.teamstats.GOOD, color: colors.slate["700"] },
+            { value: props.teamstats.WARNING, color: colors.yellow["300"] },
+            { value: props.teamstats.FAILURE, color: colors.slate["700"] },
+          ]}
+          lineWidth="10"
+          animate={true}
+        >
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="-10"
+            textAnchor="middle"
+            className="fill-warning text-xl"
+          >
+            {props.teamstats.WARNING}
+          </text>
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="10"
+            textAnchor="middle"
+            className=" fill-warning/50 text-xs font-light"
+          >
+            WARNING
+          </text>
+        </PieChart>
+      </PieChart>
+      <PieChart
+        data={[
+          { value: props.teamstats.GOOD, color: "rgba(51, 65, 85, 0.5)" },
+          { value: props.teamstats.WARNING, color: "rgba(51, 65, 85, 0.5)" },
+          { value: props.teamstats.FAILURE, color: "rgba(239, 68, 68, 0.5)" },
+        ]}
+        lineWidth="25"
+        animate={true}
+      >
+        <PieChart
+          data={[
+            { value: props.teamstats.GOOD, color: colors.slate["700"] },
+            { value: props.teamstats.WARNING, color: colors.slate["700"] },
+            { value: props.teamstats.FAILURE, color: colors.red["500"] },
+          ]}
+          lineWidth="10"
+          animate={true}
+        >
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="-10"
+            textAnchor="middle"
+            className="fill-failure text-xl"
+          >
+            {props.teamstats.FAILURE}
+          </text>
+          <text
+            dominantBaseline="central"
+            x="50"
+            y="50"
+            dy="10"
+            textAnchor="middle"
+            className=" fill-failure/75 text-xs font-light"
+          >
+            FAILURE
+          </text>
+        </PieChart>
+      </PieChart>
+    </div>
+  );
 };
 export default DonutCharts;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,71 +1,105 @@
-import { BsGithub, BsFillDatabaseFill } from 'react-icons/bs';
-import { RiNotificationFill, RiTeamFill } from 'react-icons/ri';
-import { HiDocumentText } from 'react-icons/hi';
-import { MdDeveloperMode } from 'react-icons/md';
+import { BsGithub, BsFillDatabaseFill } from "react-icons/bs";
+import { RiNotificationFill, RiTeamFill } from "react-icons/ri";
+import { HiDocumentText } from "react-icons/hi";
+import { MdDeveloperMode } from "react-icons/md";
 
-const SideBar = props => {
-	return (
-		<ul className="hidden h-full w-max flex-col lg:flex lg:w-1/5">
-			<a href="/">
-				<li className={`sidebarTab rounded-lg ${!(props.currentNav === 'System Log') ? 'bg-slate-700 text-gray-200' : ''}`}>
-					<h3>
-						<RiTeamFill className="mr-2 inline" aria-hidden="true" />
-						All Teams
-					</h3>
-				</li>
-			</a>
-			<li className="pt-0">
-				<ul className="border-l">
-					{props.teams.map((team, index) => (
-						<a href={`/?show=${team.name.replace(/\s+/g, '-')}`}>
-							<li key={index} className={`sidebarTab ${props.currentNav === team.name && 'bg-slate-800 text-gray-200'}`}>
-								{team.name}
-								{props.teamstats[team.name] && props.teamstats[team.name].GOOD > 0 && <span className="ml-2 max-w-min rounded-full bg-good px-2 font-bold text-black ">{props.teamstats[team.name].GOOD}</span>}
-								{props.teamstats[team.name] && props.teamstats[team.name].WARNING > 0 && <span className="ml-2 max-w-min rounded-full bg-warning px-2 font-bold text-black ">{props.teamstats[team.name].WARNING}</span>}
-								{props.teamstats[team.name] && props.teamstats[team.name].FAILURE > 0 && <span className="ml-2 max-w-min rounded-full bg-failure px-2 font-bold text-black ">{props.teamstats[team.name].FAILURE}</span>}
-							</li>
-						</a>
-					))}
-				</ul>
-			</li>
-			<a href="/?show=system-log">
-				<li className={`sidebarTab rounded-lg ${props.currentNav === 'System Log' ? 'bg-slate-700 text-gray-200' : ''}`}>
-					<h3 className={props.currentNav === 'System Log' ? 'text-gray-200' : ''}>
-						<HiDocumentText className="mr-2 inline" aria-hidden="true" />
-						System Log
-					</h3>
-				</li>
-			</a>
-			<hr></hr>
-			<li>
-				<h3>
-					<MdDeveloperMode className="mr-2 inline" aria-hidden="true" />
-					Developer Links
-				</h3>
-			</li>
-			<li className="pt-0">
-				<ul className="border-l">
-					<li className="min-w-max">
-						<a href={`mailto:${'nathanielw@utahcounty.gov'}`}>
-							<RiNotificationFill className="mr-2 inline" aria-hidden="true" />
-							Request Notifications
-						</a>
-					</li>
-					<li className="min-w-max">
-						<a href="https://github.com/ITDeptUtahCountyGovernment/IT-aws-healthcheck-monitor-client/issues">
-							<BsGithub className="mr-2 inline" aria-hidden="true" />
-							Report any issues
-						</a>
-					</li>
-					<li className="min-w-max">
-						<a href="http://ucapphealth.com/db.sqlite">
-							<BsFillDatabaseFill className="mr-2 inline" aria-hidden="true" />
-							Download db.sqlite
-						</a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	);
+const SideBar = (props) => {
+  return (
+    <ul className="hidden h-full w-max flex-col lg:flex lg:w-1/4">
+      <a href="/">
+        <li
+          className={`sidebarTab rounded-lg ${
+            !(props.currentNav === "System Log")
+              ? "bg-slate-700 text-gray-200"
+              : ""
+          }`}
+        >
+          <h3>
+            <RiTeamFill className="mr-2 inline" aria-hidden="true" />
+            All Teams
+          </h3>
+        </li>
+      </a>
+      <li className="pt-0">
+        <ul className="border-l">
+          {props.teams.map((team, index) => (
+            <a href={`/?show=${team.name.replace(/\s+/g, "-")}`}>
+              <li
+                key={index}
+                className={`sidebarTab ${
+                  props.currentNav === team.name && "bg-slate-800 text-gray-200"
+                }`}
+              >
+                {team.name}
+                {props.teamstats[team.name] &&
+                  props.teamstats[team.name].GOOD > 0 && (
+                    <span className="ml-2 max-w-min rounded-full bg-good px-2 font-bold text-black ">
+                      {props.teamstats[team.name].GOOD}
+                    </span>
+                  )}
+                {props.teamstats[team.name] &&
+                  props.teamstats[team.name].WARNING > 0 && (
+                    <span className="ml-2 max-w-min rounded-full bg-warning px-2 font-bold text-black ">
+                      {props.teamstats[team.name].WARNING}
+                    </span>
+                  )}
+                {props.teamstats[team.name] &&
+                  props.teamstats[team.name].FAILURE > 0 && (
+                    <span className="ml-2 max-w-min rounded-full bg-failure px-2 font-bold text-black ">
+                      {props.teamstats[team.name].FAILURE}
+                    </span>
+                  )}
+              </li>
+            </a>
+          ))}
+        </ul>
+      </li>
+      <a href="/?show=system-log">
+        <li
+          className={`sidebarTab rounded-lg ${
+            props.currentNav === "System Log"
+              ? "bg-slate-700 text-gray-200"
+              : ""
+          }`}
+        >
+          <h3
+            className={props.currentNav === "System Log" ? "text-gray-200" : ""}
+          >
+            <HiDocumentText className="mr-2 inline" aria-hidden="true" />
+            System Log
+          </h3>
+        </li>
+      </a>
+      <hr></hr>
+      <li>
+        <h3>
+          <MdDeveloperMode className="mr-2 inline" aria-hidden="true" />
+          Developer Links
+        </h3>
+      </li>
+      <li className="pt-0">
+        <ul className="border-l">
+          <li className="min-w-max">
+            <a href={`mailto:${"nathanielw@utahcounty.gov"}`}>
+              <RiNotificationFill className="mr-2 inline" aria-hidden="true" />
+              Request Notifications
+            </a>
+          </li>
+          <li className="min-w-max">
+            <a href="https://github.com/ITDeptUtahCountyGovernment/IT-aws-healthcheck-monitor-client/issues">
+              <BsGithub className="mr-2 inline" aria-hidden="true" />
+              Report any issues
+            </a>
+          </li>
+          <li className="min-w-max">
+            <a href="http://ucapphealth.com/db.sqlite">
+              <BsFillDatabaseFill className="mr-2 inline" aria-hidden="true" />
+              Download db.sqlite
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
 };
 export default SideBar;
